f26bfe812e2da223d29dd6c95bcc1e1ba68f1302
lyapkin <lyapkin992@gmail.com>
2024-09-24 14:44:08 +0500

    change post slug to blank

--- visota/apps/blog/models.py
+++ visota/apps/blog/models.py
@@ -10,7 +10,7 @@ from common.utils import CustomStorage
 class Post(TranslatableModel):
     translations = TranslatedFields(
         title = models.CharField("заголовок", max_length=100, unique=True),
-        slug = models.SlugField("url", max_length=250, unique=True),
+        slug = models.SlugField("url", max_length=250, unique=True, blank=True),
         content = CKEditor5Field("текст статьи", config_name='extends2'),
         # content = RichTextField("текст статьи"),
         content_concise = models.TextField("краткое описание статьи", max_length=120),
